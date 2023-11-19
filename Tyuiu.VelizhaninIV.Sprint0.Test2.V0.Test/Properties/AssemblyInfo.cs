95efd475aeea447ed0c4566fd2ecdf2c59063b84
Ilya Velizhanin <Tyuiu.VelizhaninIV@outlook.com>
2023-11-19 13:47:21 +0500

    Task2

---
+++ Tyuiu.VelizhaninIV.Sprint0.Test2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VelizhaninIV.Sprint0.Test2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VelizhaninIV.Sprint0.Test2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00ce13e2-16ed-420b-8429-127ad3323047")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
